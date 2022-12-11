cedcfa58b03f6eaf9e9ae7e5b5af990b934d08fb
LynnLi <31783434+xiaoxiaoli123@users.noreply.github.com>
2022-12-11 13:28:59 +0100

    Add files via upload

--- Plane fighting.cpp
+++ Plane fighting.cpp
@@ -26,10 +26,11 @@
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
-enum Screen //设置窗口大小
+enum General //设置窗口大小
 {
     screenWidth = 480,
-    screenHeight = 700
+    screenHeight = 700,
+    BULLET_NUM = 15//玩家子弹的数量
 };
 
 struct Player
@@ -37,13 +38,57 @@ struct Player
     int x;
     int y;
     bool live;//是否存活
-}player;//结构体别名
+}player, bullet[BULLET_NUM];//player结构体别名, 子弹直接放在这里
 
 void gameInit()//游戏初始化
 {
     player.x = screenWidth / 2 - 60;
     player.y = screenHeight - 120;
     player.live = true;
+
+    //初始化子弹的状况
+    for (int i = 0; i < BULLET_NUM; i++)
+    {
+        //初始bullet开始的状态，如果是false的话没必要去画它
+        bullet[i].x = 0;
+        bullet[i].y = 0;
+        bullet[i].live = false;
+    }
+}
+
+//创建子弹(因为后面玩家要一按键就创建一个子弹，playerMove里要引用这个函数，所以放在playerMove之前)
+void createBullet()
+{
+    for (int i = 0; i < BULLET_NUM; i++)
+    {
+        //如果子弹死了才能重新从飞机头部创建
+        for (int i = 0; i < BULLET_NUM; i++)
+        {
+            if (!bullet[i].live)
+            {
+                bullet[i].x = player.x + 50;
+                bullet[i].y = player.y;
+                bullet[i].live = true;
+                break; //按一次键发射一次
+            }
+        }
+    }
+}
+
+//让子弹移动
+void bulletMove()
+{
+    for (int i = 0; i < BULLET_NUM; i++)
+    {
+        if (bullet[i].live) //子弹活着的时候才会动
+        {
+            bullet[i].y -= 10;//只需要y轴的变化
+            if (bullet[i].y < 0) //如果子弹飞出屏幕，那么它就死了，这样才能创建出新的子弹
+            {
+                bullet[i].live = false;
+            }
+        }
+    }
 }
 
 //角色移动，用键盘上、下、左、右键控制
@@ -51,21 +96,43 @@ void playerMove(int speed) //可以通过speed调节飞机速度
 {
     if (IsKeyDown(KEY_UP))
     {
-        player.y -= speed;
+        //处理边界，不让飞机飞到屏幕外去
+        if (player.y > 0)
+        {
+            player.y -= speed;
+        }
+        
     }
     if (IsKeyDown(KEY_DOWN))
     {
-        player.y += speed;
+        if (player.y < (screenHeight - 120))
+        {
+            player.y += speed;
+        }
     }
     if (IsKeyDown(KEY_LEFT))
     {
-        player.x -= speed;
+        if (player.x > -30)
+        {
+            player.x -= speed;
+        }
     }
     if (IsKeyDown(KEY_RIGHT))
     {
-        player.x += speed;
+        if (player.x < (screenWidth - 80))
+        {
+            player.x += speed; 
+        }
+    }
+
+    if (IsKeyPressed(KEY_SPACE))
+    {
+        SetTargetFPS(200);//调整子弹的速度
+        createBullet();
     }
 }
+
+
 
 int main(void)
 {
@@ -77,6 +144,7 @@ int main(void)
     // 加载图片，注意要用texture
     Texture tbk = LoadTexture("background.png");//背景
     Texture tplayer = LoadTexture("me1.png");//玩家
+    Texture tbullet = LoadTexture("bullet1.png");//子弹
     
     
     
@@ -99,6 +167,15 @@ int main(void)
         DrawTexture(tbk, 0, 0, WHITE);
         DrawTexture(tplayer, player.x, player.y, WHITE);
         playerMove(5);
+        //绘制子弹，注意要判断为true时才绘制
+        for (int i = 0; i < BULLET_NUM; i++)
+        {
+            if (bullet[i].live)
+            {
+                DrawTexture(tbullet, bullet[i].x, bullet[i].y, WHITE);
+            }
+        }
+        bulletMove();
 
         ClearBackground(RAYWHITE);
 
